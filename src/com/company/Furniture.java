0b80b7e7e0725338a82652e0284f27377e59ab7f
Pavlov Vitaliy <gipsonpv@gmail.com>
2021-04-20 09:32:26 +0300

    Task 7.2

---
+++ src/com/company/Furniture.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Furniture {
+
+     String name;
+     String color;
+     double width;
+     double length;
+     double height;
+
+    public Furniture(String name, String color, double width, double length, double height) {
+        this.name = name;
+        this.color = color;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+}
